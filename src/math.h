b4929c11f5e85d7505bfec168154bb3a1d676981
Alexander Presthus <apresthus@gmail.com>
2023-05-23 22:54:32 +0200

    math lib

---
+++ src/math.h
@@ -0,0 +1,48 @@
+#pragma once
+
+typedef struct Vec2{
+    float x;
+    float y;
+} Vec2;
+
+typedef struct Vec3{
+    float x;
+    float y;
+    float z;
+} Vec3;
+
+typedef struct Vec4{
+    float x;
+    float y;
+    float z;
+    float w;
+} Vec4;
+
+
+Vec2 vec2(float x, float y){
+    Vec2 v;
+    v.x = x;
+    v.y = y;
+    return v;
+}
+Vec2 vec2s(float s){
+    return vec2(s, s);
+}
+Vec2 vec2_add(Vec2 a, Vec2 b){
+    return vec2(a.x + b.x, a.y + b.y);
+}
+Vec2 vec2_sub(Vec2 a, Vec2 b){
+    return vec2_add(a, vec2s(-1.0f));
+}
+Vec2 vec2_mul(Vec2 a, Vec2 b){
+    return vec2(a.x * b.x, a.y * b.y);
+}
+Vec2 vec2_div(Vec2 a, Vec2 b){
+    return vec2(a.x / b.x, a.y / b.y);
+}
+Vec2 vec2_div(Vec2 a, Vec2 b){
+    return vec2(a.x / b.x, a.y / b.y);
+}
+Vec2 vec2_scale(Vec2 a, float s){
+    return vec2(a.x * s, a.y * s);
+}
